71e472c144118af07c88197265540a100ee91f8e
Decrite <a309.alex@gmail.com>
2024-03-23 11:48:09 +0100

    chore: fix

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors')
-const {deletePictureFromLocalStorage, loadPicturesFromLocalStorage}  = require('fileHelper')
 
 // App constants
 const port = process.env.PORT || 3000;
@@ -10,6 +9,36 @@ const apiPrefix = '/api';
 
 
 let pictures = [];
+
+
+const fs = require('fs');
+const path = require('path');
+
+
+function deletePictureFromLocalStorage(id) {
+    const picturePath = path.join(__dirname, 'pictures', `${id}.png`);
+    fs.unlink(picturePath, err => {
+      if (err) {
+        console.error('Error deleting picture:', err);
+      } else {
+        console.log('Picture deleted successfully.');
+      }
+    });
+  }
+  
+  function loadPicturesFromLocalStorage() {
+    const picturesDirectory = path.join(__dirname, 'pictures');
+    fs.readdir(picturesDirectory, (err, files) => {
+      if (err) {
+        console.error('Error loading pictures:', err);
+      } else {
+        pictures = files.map(file => ({ id: path.parse(file).name }));
+        console.log('Pictures loaded successfully.');
+      }
+    });
+  }
+  
+
 
 function generateRandomString(length) {
   const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
