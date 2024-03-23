58e7bbc7acc01df8b7175bac9744b2fac67f8700
Decrite <a309.alex@gmail.com>
2024-03-23 14:32:45 +0100

    .

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const multer = require("multer");
+const path = require("path")
 
 // App constants
 const port = process.env.PORT || 3000;
