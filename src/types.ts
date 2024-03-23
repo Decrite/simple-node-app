681c694f74d22c8f15cf0f524af5f61bedfecaff
Decrite <a309.alex@gmail.com>
2024-03-23 10:55:54 +0100

    change index location

--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,6 @@
-export type GameVariant =
-  | 'standard'
-  | 'chess960'
-  | 'crazyhouse'
-  | 'antichess'
-  | 'atomic'
-  | 'horde'
-  | 'kingOfTheHill'
-  | 'racingKings'
-  | 'threeCheck'
-  | 'fromPosition'
 
-export type ChallengeAI = {
-  level?: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
-  clockLimit?: number
-  clockIncrement?: number
-  days?: 1 | 2 | 3 | 5 | 7 | 10 | 14
-  color?: 'white' | 'black' | 'random'
-  variant?: GameVariant
-  fen?: string
+
+export type Picture = {
+  data: Blob
+  id: string
 }
